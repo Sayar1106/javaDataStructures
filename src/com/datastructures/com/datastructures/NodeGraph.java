ee7d17326f922fccf9e6067f2da63be0478213e1
Sayar1106 <sayarbanerjee@gmail.com>
2022-05-01 16:52:22 +0530

    Adding BFS

--- src/com/datastructures/com/datastructures/NodeGraph.java
+++ src/com/datastructures/com/datastructures/NodeGraph.java
@@ -27,6 +27,31 @@ public class NodeGraph{
         DFSUtil(n, visited);
     }
 
+	public void BFS(Node n){
+		boolean visited[] = new boolean[V];
+		Node prev[] = new Node[V];
+		BFSUtil(n, visited, prev);
+	}
+
+	private void BFSUtil(Node n, boolean visited[], Node prev[]){
+		Queue<Node> queue = new LinkedList();
+        System.out.println("Node" + n.val + "has been reached");
+		queue.add(n);
+		visited[n.val] = true;
+		while(!queue.isEmpty()){
+			Node curr = queue.remove();
+			Iterator<Node> iter = adj[curr.val].listIterator();
+			while(iter.hasNext()){
+				Node next = iter.next();
+				if(!visited[next.val]){
+					queue.add(next);
+					visited[next.val] = true;
+					System.out.println("Node" + next.val + "has been reached");
+					prev[next.val] = curr;
+				}
+			}
+		}
+	}
     private void DFSUtil(Node n, boolean visited[]){
         visited[n.val] = true;
         System.out.println("Node" + n.val + "has been reached");
@@ -39,12 +64,25 @@ public class NodeGraph{
         }
     }
     public static void main(String args[]){
-        NodeGraph graph = new NodeGraph(3);
-        Node zero = new Node(0);
-        Node one = new Node(1);
-        Node two = new Node(2);
-        graph.addEdge(one, two);
-        graph.addEdge(one, zero);
-        graph.DFS(one);
+        NodeGraph graph = new NodeGraph(13);
+        Node nodeList[] = new Node[13];
+        for(int i = 0; i < nodeList.length; i++){
+            nodeList[i] = new Node(i);
+        }
+        graph.addEdge(nodeList[0], nodeList[9]);
+        graph.addEdge(nodeList[0], nodeList[7]);
+        graph.addEdge(nodeList[0], nodeList[11]);
+        graph.addEdge(nodeList[9], nodeList[10]);
+        graph.addEdge(nodeList[9], nodeList[8]);
+        graph.addEdge(nodeList[7], nodeList[3]);
+        graph.addEdge(nodeList[7], nodeList[6]);
+        graph.addEdge(nodeList[10], nodeList[1]);
+        graph.addEdge(nodeList[8], nodeList[1]);
+        graph.addEdge(nodeList[8], nodeList[12]);
+        graph.addEdge(nodeList[3], nodeList[2]);
+        graph.addEdge(nodeList[3], nodeList[4]);
+        graph.addEdge(nodeList[6], nodeList[5]);
+        graph.addEdge(nodeList[12], nodeList[2]);
+        graph.BFS(nodeList[0]);
     }
 }
